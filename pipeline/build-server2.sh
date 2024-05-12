33dc4fa7c7f881b26b87265e8d8a7fd6bd4456b7
LafTools Team - FX <work7z@outlook.com>
2024-05-12 12:29:24 +0800

    chore: Update package.json version to v3.1.49

--- pipeline/build-server2.sh
+++ pipeline/build-server2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ver=$1 # example: ver=v3.5.$(date +%Y%m%d%H%M%S)
+ver=$1 
 set +e
 cd $MDGJX_ROOT/modules/server2
 chmod +x ./docker/*.sh
 echo "[I] $(date) Building server2..."
 docker build -t localbuild/server2-linux-x64:$ver -f ./Dockerfile .
 # chmod +x ./docker/run-docker.sh
-# ./docker/run-docker.sh server2-inst $ver
+# ./docker/run-docker.sh server2-inst $ver 
