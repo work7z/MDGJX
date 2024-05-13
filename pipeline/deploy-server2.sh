0a7885c62f0f417bc75435eb08d372ee6c060aa0
LafTools Team - FX <work7z@outlook.com>
2024-05-13 22:33:10 +0800

    chore: Update deploy-server2.sh to run docker script for server2-inst

--- pipeline/deploy-server2.sh
+++ pipeline/deploy-server2.sh
@@ -4,4 +4,4 @@ source ~/.zshrc
 set +e
 cd $MDGJX_ROOT/modules/server2
 ssh $SERVER_2H4G -p 26609 "echo 'hello, production'"
-# ssh $SERVER_2H4G -p 26609 "/home/appuser/dkplace/run-docker-$ver.sh server2-inst $ver"
+ssh $SERVER_2H4G -p 26609 "/home/appuser/dkplace/run-docker-$ver.sh server2-inst $ver"
