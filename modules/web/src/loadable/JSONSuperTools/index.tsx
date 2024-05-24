984ded9c0d8f652aad8be73035c2544f8a2f8f1f
LafTools Team - FX <work7z@outlook.com>
2024-05-25 01:25:39 +0800

    chore: Update JSONSuperTools component with new icons and functionality

--- modules/web/src/loadable/JSONSuperTools/index.tsx
+++ modules/web/src/loadable/JSONSuperTools/index.tsx
@@ -1,32 +1,62 @@
 import LoadableWrapper from "@/components/LoadableWrapper";
 import { Card, Tabs, rem } from "@mantine/core"
-import { IconGitCompare, IconLayersIntersect, IconMessageCircle, IconPhoto, IconSettings } from "@tabler/icons-react"
-
+import { IconDatabaseExport, IconFileInfinity, IconGitCompare, IconInfinity, IconLayersIntersect, IconMessageCircle, IconOutlet, IconPhoto, IconSettings, IconTransform, IconTransformFilled } from "@tabler/icons-react"
+import { Switch } from "react-router";
+export type JSONTypeItem = {
+    name: string,
+    id: string,
+    icon: any,
+    loadFN: any
+}
 export default () => {
 
     const iconStyle = { width: rem(12), height: rem(12) };
-
+    const items: JSONTypeItem[] = [
+        {
+            name: '格式转换',
+            id: 'conversion',
+            icon: IconTransformFilled,
+            loadFN: () => import('./JSONConversion.tsx')
+        },
+        {
+            name: '格式提取',
+            id: 'export',
+            icon: IconOutlet,
+            loadFN: () => import('./JSONExport.tsx')
+        },
+        {
+            name: '差异对比',
+            id: 'comparison',
+            icon: IconFileInfinity,
+            loadFN: () => import('./JSONDiff.tsx')
+        },
+    ]
     return (
         <Card p={0} withBorder style={{
             minHeight: 'calc(100vh - 85px)'
         }}>
             <Tabs defaultValue="conversion">
                 <Tabs.List>
-                    <Tabs.Tab value="conversion" leftSection={<IconLayersIntersect style={iconStyle} />}>
-                        格式转换
-                    </Tabs.Tab>
-                    <Tabs.Tab value="comparison" leftSection={<IconGitCompare style={iconStyle} />}>
-                        差异对比
-                    </Tabs.Tab>
+                    {
+                        items.map(x => {
+                            return (
+                                <Tabs.Tab value={x.id} leftSection={<x.icon style={iconStyle} />}>
+                                    {x.name}
+                                </Tabs.Tab>
+                            )
+                        })
+                    }
                 </Tabs.List>
 
-                <Tabs.Panel value="conversion">
-                    <LoadableWrapper fn={() => import('./JSONConversion.tsx')} />
-                </Tabs.Panel>
-
-                <Tabs.Panel value="comparison">
-                    <LoadableWrapper fn={() => import('./JSONDiff.tsx')} />
-                </Tabs.Panel>
+                {
+                    items.map(x => {
+                        return (
+                            <Tabs.Panel value={x.id}>
+                                <LoadableWrapper fn={x.loadFN} />
+                            </Tabs.Panel>
+                        )
+                    })
+                }
             </Tabs>
         </Card>
     )
