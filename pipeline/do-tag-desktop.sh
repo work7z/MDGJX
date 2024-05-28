fdd34d0882f2f8128b7677f35f61396e0cddd454
LafTools Team - FX <work7z@outlook.com>
2024-05-28 12:56:03 +0800

    chore: Update package version to desktop2-v5.3.28 in package.json

---
+++ pipeline/do-tag-desktop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git status|grep modified
+if [ $? -eq 0 ]; then
+    echo "Please commit your changes before tagging"
+    exit 1
+fi
+
+set -e
+version=`jq '.version' $MDGJX_ROOT/modules/desktop2/package.json -r` 
+tagName=$version
+git tag $tagName
+git push origin $tagName
