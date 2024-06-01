9ed96585dd1c258c7cf5980fddb987d8012a6812
LafTools Team - FX <work7z@outlook.com>
2024-06-01 13:37:49 +0800

    chore: Update package.json version to v3.2.40 and build script to include node_modules in dist folder

--- pipeline/build-fe.sh
+++ pipeline/build-fe.sh
@@ -24,7 +24,9 @@ echo "[I] building fe"
     [ -d node_modules ] && rm -rf node_modules
     [ ! -d node_modules ] && npm i --omit=dev --force  
 
-    cp -a ./node_modules/ ./dist/
+    mkdir ./dist/node_modules
+
+    cp -a ./node_modules/* ./dist/node_modules/
 
     echo "[I] building web-server"
     [ -d node_modules ] && rm -rf node_modules
