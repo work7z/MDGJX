c91574c14bad116611355faff928e7ff4ddbfeea
LafTools Team - FX <work7z@outlook.com>
2024-06-06 00:17:54 +0800

    chore: Update desktop2 version to v5.4.21 in package.json

--- pipeline/tools/get-desktop2-version.sh
+++ pipeline/tools/get-desktop2-version.sh
@@ -5,4 +5,7 @@ if [ "$P_OVERRIDED_VERSION" = "" ]; then
     P_OVERRIDED_VERSION=$(node -pe 'require("./version.json").desktop2')
 fi
 
-echo $P_OVERRIDED_VERSION | sed 's/desktop2-//g'
+newVer=`echo $P_OVERRIDED_VERSION | sed 's/desktop2-//g'`
+echo $newVer
+# for package.json, replace PLEASE_REPLACE_VERSION to newVer
+sed -i '' "s/PLEASE_REPLACE_VERSION/$newVer/g" $MDGJX_ROOT/modules/desktop2/package.json
--- pipeline/upload-desktop2.sh
+++ pipeline/upload-desktop2.sh
@@ -4,7 +4,7 @@ TAGNAME=$2
 
 cd $MDGJX_ROOT/modules/desktop2/pkg-dist
 finFolder=/home/appuser/dkstatic/$uploadType/desktop2
-ssh $SERVER_2H2G -p 26609 "mkdir -p $finFolder" 
+# ssh $SERVER_2H2G -p 26609 "mkdir -p $finFolder" 
 # sftp -P 26609  $SERVER_2H2G <<< "put -r ./MDGJX-* $finFolder"
 source ~/.zshrc
 mkdir -p ./$TAGNAME
