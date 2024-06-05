a4e617636badb3c29f8c2ca336cc3b0e3d3fb494
LafTools Team - FX <work7z@outlook.com>
2024-06-05 12:49:17 +0800

    chore: Update get-desktop2-version.sh to replace "desktop2-" in the version string

--- pipeline/tools/get-desktop2-version.sh
+++ pipeline/tools/get-desktop2-version.sh
@@ -4,4 +4,8 @@ P_OVERRIDED_VERSION=$OVERRIDED_VERSION
 if [ "$P_OVERRIDED_VERSION" = "" ]; then
     P_OVERRIDED_VERSION=$(node -pe 'require("./version.json").desktop2')
 fi
-echo $P_OVERRIDED_VERSION
+
+# replace desktop2 to empty
+
+
+echo $P_OVERRIDED_VERSION | sed 's/desktop2-//g'
