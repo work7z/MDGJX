7ca09e390737ae75e5399969f889f971596c2172
LafTools Team - FX <work7z@outlook.com>
2024-06-25 23:06:34 +0800

    refactor: Update moduleItemQuery property in SystemSubModuleItem type

--- modules/web-server/src/m-types-copy/base/m-types-main.ts
+++ modules/web-server/src/m-types-copy/base/m-types-main.ts
@@ -40,7 +40,7 @@ export type SystemSubModuleItem = {
   seoName?: string;
   disableFooter?: boolean;
   moduleItemtURL?:string;
-  moduleItemQuery: any;
+  moduleItemQuery?: any;
   defaultSubToolId?: string;
   children?: SystemSubModuleItem[];
   keywords?: string[];
