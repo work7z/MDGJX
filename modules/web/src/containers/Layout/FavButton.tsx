d73b82b267bea0adb5997f4e915af777e6b8b158
LafTools Team - FX <work7z@outlook.com>
2024-07-03 23:40:33 +0800

    refactor: Update version.json to v5.4.96 for web2 and desktop2

--- modules/web/src/containers/Layout/FavButton.tsx
+++ modules/web/src/containers/Layout/FavButton.tsx
@@ -6,88 +6,92 @@ import exportUtils from "@/utils/ExportUtils";
 import { ActionIcon, Affix, Button, Tooltip, Transition, rem } from "@mantine/core"
 import { useWindowScroll } from "@mantine/hooks";
 import { IconArrowUp, IconBookmark, IconPlaylist, IconPlaylistAdd } from "@tabler/icons-react"
+import _ from "lodash";
 import { useState } from "react";
 
 export default () => {
+    const signIn = useHasUserSignIn()
+    const rh = exportUtils.register('favbtn', {
+        getNotPersistedStateFn() {
+            return {}
+        },
+        getPersistedStateFn() {
+            return {}
+        }
+    })
+    const folderRes = apiSlice.useFavFoldersGetQuery({}, {
+        skip: !signIn,
+        pollingInterval: 1000 * 60 * 5,
+    })
+    const folderItemsRes = apiSlice.useFavFoldersItemsGetQuery({}, {
+        pollingInterval: 1000 * 60 * 5,
+        skip: !signIn
+    })
+    const [t_addFolderItem, addFolderItemRes] = apiSlice.useLazyFavFoldersItemAddQuery({})
+    const [loading, onLoading] = useState(false)
+    const [t_favFoldersItemDelete, favFoldersItemDeleteRes] = apiSlice.useLazyFavFoldersItemDeleteQuery({})
+    const currentURL = location.pathname + (
+        _.isEmpty(location.search) ? '' : '?' 
+    ) + location.search
+    const currentTitle = document.title
+    const isCurrentURLInFolder = folderItemsRes.data?.data?.some(v => v.url === currentURL)
 
-    if (true) {
-        // NOTE: FavButton 将会以功能右上角收藏作为替代
+    if (!rh) return ''
+
+    if (location.href.indexOf('/tools/index') !== -1
+        ||
+        location.href.indexOf('/tools/myfavour') !== -1
+        ||
+        location.href.indexOf('/collections/index') !== -1
+    ) {
         return ''
     }
 
-    
-    // const signIn = useHasUserSignIn()
-    // const rh = exportUtils.register('favbtn', {
-    //     getNotPersistedStateFn() {
-    //         return {}
-    //     },
-    //     getPersistedStateFn() {
-    //         return {}
-    //     }
-    // })
 
-    // const folderRes = apiSlice.useFavFoldersGetQuery({}, {
-    //     skip: !signIn,
-    //     pollingInterval: 1000 * 60 * 5,
-    // })
-    // const folderItemsRes = apiSlice.useFavFoldersItemsGetQuery({}, {
-    //     pollingInterval: 1000 * 60 * 5,
-    //     skip: !signIn
-    // })
-    // const [t_addFolderItem, addFolderItemRes] = apiSlice.useLazyFavFoldersItemAddQuery({})
-    // const [loading, onLoading] = useState(false)
-    // const [t_favFoldersItemDelete, favFoldersItemDeleteRes] = apiSlice.useLazyFavFoldersItemDeleteQuery({})
-    // const currentURL = location.pathname + '?' + location.search
-    // const currentTitle = document.title
-    // const isCurrentURLInFolder = folderItemsRes.data?.data?.some(v => v.url === currentURL)
+    return <Affix position={{ bottom: 5, right: 5 }}>
+        <Tooltip label={signIn ? isCurrentURLInFolder ? '取消收藏夹' : "加入收藏夹" : '请先登录'}>
+            <ActionIcon
+                color='green'
+                size='md'
+                variant={isCurrentURLInFolder ? 'filled' : "light"}
+                style={{ width: rem(16), height: rem(16) }}
+                onClick={async () => {
+                    onLoading(true)
+                    try {
+                        await rh.checkLoginStatus()
+                        const folder = folderRes.data?.data?.[0]
+                        if (!folder || !folder.id) {
+                            AlertUtils.alertErr('请先创建一个收藏夹')
+                        } else {
+                            if (isCurrentURLInFolder) {
+                                for (let ea of (folderItemsRes?.data?.data || [])) {
+                                    if (ea.url === currentURL) {
+                                        await t_favFoldersItemDelete({
+                                            id: ea.id + ''
+                                        })
+                                    }
+                                }
+                            } else {
+                                const r = await t_addFolderItem({
+                                    folderId: folder.id,
+                                    url: currentURL,
+                                    name: currentTitle,
+                                    remarks: '',
+                                    type: 'user'
+                                })
+                            }
+                            await folderRes.refetch()
+                            await folderItemsRes.refetch()
+                        }
+                    } finally {
+                        onLoading(false)
+                    }
+                }}
+                loading={loading || favFoldersItemDeleteRes.isFetching || addFolderItemRes.isFetching || folderRes.isLoading || folderItemsRes.isLoading}
+            >
+                <IconBookmark style={{ width: rem(16), height: rem(16) }} />
+            </ActionIcon>
+        </Tooltip>
 
-    // if (!rh) return ''
-
-
-    // return <Affix position={{ bottom: 5, right: 5 }}>
-    //     <Tooltip label={signIn ? isCurrentURLInFolder ? '取消收藏夹' : "加入收藏夹" : '请先登录'}>
-    //         <ActionIcon
-    //             color='green'
-    //             size='md'
-    //             variant={isCurrentURLInFolder ? 'filled' : "light"}
-    //             style={{ width: rem(16), height: rem(16) }}
-    //             onClick={async () => {
-    //                 onLoading(true)
-    //                 try {
-    //                     await rh.checkLoginStatus()
-    //                     const folder = folderRes.data?.data?.[0]
-    //                     if (!folder || !folder.id) {
-    //                         AlertUtils.alertErr('请先创建一个收藏夹')
-    //                     } else {
-    //                         if (isCurrentURLInFolder) {
-    //                             for (let ea of (folderItemsRes?.data?.data || [])) {
-    //                                 if (ea.url === currentURL) {
-    //                                     await t_favFoldersItemDelete({
-    //                                         id: ea.id + ''
-    //                                     })
-    //                                 }
-    //                             }
-    //                         } else {
-    //                             const r = await t_addFolderItem({
-    //                                 folderId: folder.id,
-    //                                 url: currentURL,
-    //                                 name: currentTitle,
-    //                                 remarks: '',
-    //                                 type: 'user'
-    //                             })
-    //                         }
-    //                         await folderRes.refetch()
-    //                         await folderItemsRes.refetch()
-    //                     }
-    //                 } finally {
-    //                     onLoading(false)
-    //                 }
-    //             }}
-    //             loading={loading || favFoldersItemDeleteRes.isFetching || addFolderItemRes.isFetching || folderRes.isLoading || folderItemsRes.isLoading}
-    //         >
-    //             <IconBookmark style={{ width: rem(16), height: rem(16) }} />
-    //         </ActionIcon>
-    //     </Tooltip>
-
-    // </Affix>
+    </Affix>
 }
